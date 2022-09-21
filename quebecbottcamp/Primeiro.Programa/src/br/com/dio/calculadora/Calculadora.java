3dfe61d45e5c9b75bf8337655b715a6bb6e8ffcb
GDZINN <cordeiromarcos60@gmail.com>
2022-09-20 22:08:31 -0300

    criacao de calculadora

--- quebecbottcamp/Primeiro.Programa/src/br/com/dio/calculadora/Calculadora.java
+++ quebecbottcamp/Primeiro.Programa/src/br/com/dio/calculadora/Calculadora.java
@@ -17,7 +17,7 @@ public class Calculadora {
 		int soma = soma(a,b);
 	    int subtracao = subtracao(a,b);
 	    int multiplicacao = multiplicacao(a,b);
-	    int divisao = divisao(a,b);
+	    float divisao = divisao(a,b);
 	    
 	    System.out.println("soma" + soma);
 	    System.out.println("sub" + subtracao);
@@ -33,7 +33,7 @@ public class Calculadora {
 			return a - b;
 	 }
 		
-		public static int divisao(int a, int b) {
+		public static float divisao(float a, float b) {
 			return a / b;
 	 }
 		
